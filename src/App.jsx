ffdca2599b33ef21dad670b7ead0d0948064f7bf
Success Obasi <obasisuccess192@gmail.com>
2024-01-09 12:02:17 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,127 @@
+import React from 'react'
+
+
+function App() {
+  
+  return (
+    <div className>
+      <header className='  bg-Dark-Blue-intro' >
+        <div className='flex p-5  justify-between items-center '>
+          <img src="./images/logo.svg" alt="" className=
+          'w-28'/>
+          <nav>
+            <ul className='flex space-x-5 text-white text-sm'
+            >
+              <li>Features</li>
+              <li>Team</li>
+              <li>Sign In</li>
+            </ul>
+          </nav>
+        </div>
+        
+        
+      </header>
+
+
+      <main className=''>
+      <img src="./images/illustration-intro.png" alt="" className='mt-8 px-5'/>
+        <div className='text-center '>
+          <h1 className='
+           font-raleway font-bold text-2xl text-white px-5  leading-9'>All your files in one secure location, accessible anywhere.</h1>
+
+          <p className=' text-white px-7 py-2'>
+          Fylo stores all your most important files in one secure location. Access them wherever 
+          you need, share and collaborate with friends family, and co-workers.
+          </p>
+
+          <button>Get Started</button>
+        </div>
+      </main>
+    </div>
+
+   
+      
+  
+  
+
+ 
+
+ 
+
+  // Get Started
+
+  // Access your files, anywhere
+
+  // The ability to use a smartphone, tablet, or computer to access your account means your 
+  // files follow you everywhere.
+
+  // Security you can trust
+
+  // 2-factor authentication and user-controlled encryption are just a couple of the security 
+  // features we allow to help secure your files.
+
+  // Real-time collaboration
+
+  // Securely share files and folders with friends, family and colleagues for live collaboration. 
+  // No email attachments required.
+
+  // Store any type of file
+
+  // Whether you're sharing holidays photos or work documents, Fylo has you covered allowing for all 
+  // file types to be securely stored and shared.
+
+  // Stay productive, wherever you are
+
+  // Never let location be an issue when accessing your files. Fylo has you covered for all of your file 
+  // storage needs.
+
+  // Securely share files and folders with friends, family and colleagues for live collaboration. No email 
+  // attachments required.
+
+  // See how Fylo works
+
+  // Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has 
+  // become a well-oiled collaboration machine.
+
+  // Satish Patel
+  // Founder & CEO, Huddle
+
+  // Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has 
+  // become a well-oiled collaboration machine.
+
+  // Bruce McKenzie
+  // Founder & CEO, Huddle
+
+  // Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has 
+  // become a well-oiled collaboration machine.
+
+  // Iva Boyd
+  // Founder & CEO, Huddle
+
+  // Get early access today
+
+  // It only takes a minute to sign up and our free starter tier is extremely generous. If you have any 
+  // questions, our support team would be happy to help you.
+
+  // Get Started For Free
+
+  // Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+  // dolore magna aliqua
+
+  // +1-543-123-4567
+  // example@fylo.com
+
+  // About Us
+  // Jobs
+  // Press
+  // Blog
+
+  // Contact Us
+  // Terms
+  // Privacy */}
+
+  //   // </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'raleway': ['Raleway', 'sans-serif'],
+        'open-sans': ['Open Sans', 'sans-serif'],
+      },
+      backgroundImage: {
+        'curvy-mobile': "url('./images/bg-curvy-mobile.svg')",
+        'curvy-desktop': "url('./images/bg-curvy-desktop.svg')",
+        'quotes': "url('./images/bg-quotes.png')",
+      },
+      letterSpacing: {
+        widest: "0.5em",
+      },
+      colors: {
+        'Dark-Blue-intro': " hsl(217, 28%, 15%)",
+
+        'Dark-Blue-main': "hsl(218, 28%, 13%)",
+
+        'Dark-Blue-footer': "hsl(216, 53%, 9%)",
+
+        'Dark-Blue-testimonials': " hsl(219, 30%, 18%)",
+
+        'Cyan-grdaient': "hsl(176, 68%, 64%)",
+
+        'Blue-gradient': "hsl(198, 60%, 50%)",
+        
+        'Light-Red-error': "hsl(0, 100%, 63%)"
+      },
+      backgroundPosition:{
+        'center-bottom': 'center 80%',
+      }
+    },
+  },
+  plugins: [],
+}
+
