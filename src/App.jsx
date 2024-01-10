6b2688a92036b45669073a5d19bad5884260b445
Success Obasi <obasisuccess192@gmail.com>
2024-01-11 00:33:40 +0100

    Python

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
 
 
       <main className=''>
-        <section className='hero flex items-center flex-col justify-center min-h-full text-center bg-no-repeat bg-center lg:bg-curvy-desktop lg:bg-bottom z-20'> 
+        <section className='hero flex items-center flex-col justify-center min-h-full text-center bg-no-repeat bg-center lg:bg-curvy-desktop lg:bg-bottom z-20 py-4'> 
           <img src="./images/illustration-intro.png" alt="" className='px-5 z-20' /> 
 
           <h1 className='
@@ -34,7 +34,95 @@ function App() {
           </p>
 
           <button className='bg-Cyan-grdaient text-white p-3 rounded-3xl w-60 z-20'>Get Started</button>
+        </section>
 
+        <section className=' bg-Dark-Blue-main text-center'>
+          <div className='pt-32 flex items-center justify-center flex-col text-white'>
+            <img src="/images/icon-access-anywhere.svg" alt="" />
+            <h2 className='text-xl pt-10 font-bold'>Access your files, anywhere</h2>
+            <p className='px-5 py-3'>The ability to use a smartphone, tablet, or computer to access your account means your 
+           files follow you everywhere.</p>
+          </div>
+        </section>
+
+        <section className=' bg-Dark-Blue-main text-center'>
+          <div className='pt-24 flex items-center justify-center flex-col text-white'>
+            <img src="/images/icon-security.svg" alt=""/>
+            <h2 className='text-xl pt-10 font-bold'>Security you can trust</h2>
+            <p className='px-5 py-3'>2-factor authentication and user-controlled encryption are just a couple of the security features we allow to help secure your files.</p>
+          </div>
+        </section>
+
+        <section className=' bg-Dark-Blue-main text-center'>
+          <div className='pt-24 flex items-center justify-center flex-col text-white'>
+            <img src="/images/icon-collaboration.svg" alt="" />
+            <h2 className='text-xl pt-10 font-bold'>Real-time collaboration</h2>
+            <p className='px-5 py-3'>Securely share files and folders with friends, family and colleagues for live collaboration. 
+            No email attachments required.
+            </p>
+          </div>
+        </section>
+        <section className=' bg-Dark-Blue-main text-center'>
+          <div className='pt-24 flex items-center justify-center flex-col text-white '>
+            <img src="/images/icon-any-file.svg" alt="" />
+            <h2 className='text-xl pt-10 font-bold'>Store any type of file</h2>
+            <p className='px-5 py-3'> Whether you're sharing holidays photos or work documents, Fylo has you covered allowing for all 
+            file types to be securely stored and shared.</p>
+          </div>
+        </section>
+        <section className=' bg-Dark-Blue-main text-start'>
+          <div className='pt-32 flex  flex-col text-white px-5'>
+            <img src="/images/illustration-stay-productive.png" alt="" />
+            <h2 className='text-xl pt-10 font-bold'>Stay productive, wherever you are</h2>
+            <p className='py-4'>Never let location be an issue when accessing your files. Fylo has you covered for all of your file 
+            storage needs.</p>
+            <p className='py-4'>
+            Securely share files and folders with friends, family and colleagues for live collaboration. No email 
+            attachments required.
+            </p>
+            <div className='text-start flex items-center space-x-2 py-2 border-b border-cyan-300 w-fit'>
+              <p className='text-cyan-300'>See how Fylo works</p>
+              <img src="/images/icon-arrow.svg" alt="" className='w-5'/>
+            </div>
+          </div>
+        </section>
+
+        <section className='testimonials flex flex-col items-center justify-center bg-Dark-Blue-main text-white pt-44 text-sm'  >
+          <div className='bg-Dark-Blue-testimonials w-10/12 p-5 flex flex-col rounded-sm mb-6'>
+            <p className='leading-6 py-4'>Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has 
+            become a well-oiled collaboration machine</p>
+            <div className='flex space-x-4 items-center'>
+              <img src="/images/profile-1.jpg" alt="" className='w-12 h-12 rounded-full' />
+              <div className='leading-6 '>
+                <h3 className='font-bold tracking-wider'>Satish Patel</h3>
+                <p className='text-xs'>Founder & CEO, Huddle</p>
+              </div>
+            </div>
+          </div>
+
+          <div className='bg-Dark-Blue-testimonials w-10/12 p-5 flex flex-col rounded-sm mb-6'>
+            <p className='leading-6 py-4'>Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has 
+            become a well-oiled collaboration machine</p>
+            <div className='flex space-x-4 items-center'>
+              <img src="/images/profile-2.jpg" alt="" className='w-12 h-12 rounded-full' />
+              <div className='leading-6 '>
+                <h3 className='font-bold tracking-wider'>Bruce McKenzie</h3>
+                <p className='text-xs'>Founder & CEO, Huddle</p>
+              </div>
+            </div>
+          </div>
+          <div className='bg-Dark-Blue-testimonials w-10/12 p-5 flex flex-col rounded-sm mb-6'>
+            <p className='leading-6 py-4'>Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has 
+            become a well-oiled collaboration machine</p>
+            <div className='flex space-x-4 items-center'>
+              <img src="/images/profile-3.jpg" alt="" className='w-12 h-12 rounded-full' />
+              <div className='leading-6 '>
+                <h3 className='font-bold tracking-wider'>Iva Boyd</h3>
+                <p className='text-xs'>Founder & CEO, Huddle</p>
+              </div>
+            </div>
+          </div>
+          
         </section>
       </main>
     </div>
@@ -48,52 +136,22 @@ function App() {
 
  
 
-  // Get Started
 
-  // Access your files, anywhere
 
-  // The ability to use a smartphone, tablet, or computer to access your account means your 
-  // files follow you everywhere.
+  
 
-  // Security you can trust
 
-  // 2-factor authentication and user-controlled encryption are just a couple of the security 
-  // features we allow to help secure your files.
 
-  // Real-time collaboration
 
-  // Securely share files and folders with friends, family and colleagues for live collaboration. 
-  // No email attachments required.
+  
 
-  // Store any type of file
 
-  // Whether you're sharing holidays photos or work documents, Fylo has you covered allowing for all 
-  // file types to be securely stored and shared.
+  
 
-  // Stay productive, wherever you are
+  // 
+  // 
 
-  // Never let location be an issue when accessing your files. Fylo has you covered for all of your file 
-  // storage needs.
 
-  // Securely share files and folders with friends, family and colleagues for live collaboration. No email 
-  // attachments required.
-
-  // See how Fylo works
-
-  // Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has 
-  // become a well-oiled collaboration machine.
-
-  // Satish Patel
-  // Founder & CEO, Huddle
-
-  // Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has 
-  // become a well-oiled collaboration machine.
-
-  // Bruce McKenzie
-  // Founder & CEO, Huddle
-
-  // Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has 
-  // become a well-oiled collaboration machine.
 
   // Iva Boyd
   // Founder & CEO, Huddle
